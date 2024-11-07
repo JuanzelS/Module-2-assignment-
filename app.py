e23aec8129e9aa148d9bf073ba737c3bbd34b4e2
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2024-11-07 15:22:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def sort_letters(message):
     and return the new string."""
     return ''.join(sorted(list(message)))
 
-
 @app.route('/')
 def homepage():
     """A homepage with handy links for your convenience."""
@@ -17,33 +16,64 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return """
+    <form action="/froyo_results" method="GET">
+        What is your favorite Fro-Yo flavor? <br/>
+        <input type="text" name="flavor"><br/>
+        What toppings would you like? <br/>
+        <input type="text" name="toppings"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
 @app.route('/froyo_results')
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
-    pass
+    flavor = request.args.get('flavor')
+    toppings = request.args.get('toppings')
+    return f"You ordered {flavor} flavored Fro-Yo with toppings {toppings}!"
 
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
-    pass
+    return """
+    <form action="/favorites_results" method="GET">
+        What's your favorite color? <br/>
+        <input type="text" name="color"><br/>
+        What's your favorite animal? <br/>
+        <input type="text" name="animal"><br/>
+        What's your favorite city? <br/>
+        <input type="text" name="city"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
 @app.route('/favorites_results')
 def favorites_results():
     """Shows the user a nice message using their form results."""
-    pass
+    color = request.args.get('color')
+    animal = request.args.get('animal')
+    city = request.args.get('city')
+    return f"Wow, I didn't know {color} {animal}s lived in {city}!"
 
-@app.route('/secret_message')
+@app.route('/secret_message', methods=['GET'])
 def secret_message():
     """Shows the user a form to collect a secret message. Sends the result via
     the POST method to keep it a secret!"""
-    pass
+    return """
+    <form action="/message_results" method="POST">
+        Enter your secret message: <br/>
+        <input type="text" name="message"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
 @app.route('/message_results', methods=['POST'])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
-    pass
+    message = request.form.get('message')
+    sorted_message = sort_letters(message)
+    return f"Here's your secret message!<br> {sorted_message}"
 
 @app.route('/calculator')
 def calculator():
@@ -66,8 +96,26 @@ def calculator():
 @app.route('/calculator_results')
 def calculator_results():
     """Shows the user the result of their calculation."""
-    pass
+    num1 = int(request.args.get('operand1'))
+    num2 = int(request.args.get('operand2'))
+    operation = request.args.get('operation')
+    
+    if operation == 'add':
+        result = num1 + num2
+        op_symbol = '+'
+    elif operation == 'subtract':
+        result = num1 - num2
+        op_symbol = '-'
+    elif operation == 'multiply':
+        result = num1 * num2
+        op_symbol = '*'
+    elif operation == 'divide':
+        result = num1 / num2
+        op_symbol = '/'
+    else:
+        return "Invalid operation selected"
 
+    return f"You chose to {op_symbol} {num1} and {num2}. Your result is: {result}"
 
 HOROSCOPE_PERSONALITIES = {
     'aries': 'Adventurous and energetic',
@@ -92,16 +140,9 @@ def horoscope_form():
 @app.route('/horoscope_results')
 def horoscope_results():
     """Shows the user the result for their chosen horoscope."""
-
-    # TODO: Get the sign the user entered in the form, based on their birthday
-    horoscope_sign = ''
-
-    # TODO: Look up the user's personality in the HOROSCOPE_PERSONALITIES
-    # dictionary based on what the user entered
-    users_personality = ''
-
-    # TODO: Generate a random number from 1 to 99
-    lucky_number = 0
+    horoscope_sign = request.args.get('sign')
+    users_personality = HOROSCOPE_PERSONALITIES.get(horoscope_sign, "Unknown sign")
+    lucky_number = random.randint(1, 99)
 
     context = {
         'horoscope_sign': horoscope_sign,
